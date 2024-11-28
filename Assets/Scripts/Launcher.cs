9f24b9020ff5e0c1428d3691139de780c054e543
fercam98 <campayo.adrian@gmail.com>
2024-11-28 19:25:57 +0100

    Agregar nueva funcionalidad

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+
+    private void Start()
+    {
+        Debug.Log("Nueva funcionalidad añadida");
+    }
+
+}
